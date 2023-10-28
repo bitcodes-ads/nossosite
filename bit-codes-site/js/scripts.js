d03231da1567e087c7c39df2d96096888d05e34f
wedisleyMateus <wedmat282016@gmail.com>
2023-10-28 17:42:14 -0300

    Implementação do Grafico e das Noticias

--- bit-codes-site/js/scripts.js
+++ bit-codes-site/js/scripts.js
@@ -52,3 +52,51 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const ctx = document.getElementById('myChart');
+    let Linguagens = ['JavaScript', 'HTML/Css', 'Python', 'SQL', 'TypeScript', 'Bash/Shell']
+    let Notas = [63, 52, 49, 48, 38, 32]
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: Linguagens,
+            datasets: [{
+                label: 'Mais Usadas',
+                data: Notas,
+                borderWidth: 1
+            }]
+         },
+        options: {
+            indexAxis: 'y', 
+        }
+    });
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entries.target.classList.remove('show')
+        }
+    })
+})
+        
+const elements = document.querySelectorAll('.hidden')
+        
+elements.forEach (element => myObserver.observe(element))
+        
+        
+const myObserver2 = new IntersectionObserver((entries2) =>{
+    entries2.forEach(entry2 => {
+        if(entry2.isIntersecting){
+            entry2.target.classList.add('show2')
+        }else{
+            entries2.target.classList.remove('show2')
+        }
+    })
+})
+        
+const elements2 = document.querySelectorAll('.hidden2')
+        
+elements2.forEach (element2 => myObserver2.observe(element2))
